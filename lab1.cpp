c6c0228d9a212e549fca0c1639d2128b60c1e124
Ihor Shcherbina <hoqqez@gmail.com>
2024-01-19 19:09:32 +0200

    edit code

--- lab1.cpp
+++ lab1.cpp
@@ -1,9 +1,13 @@
 ﻿#include <iostream>
 #include <iomanip>
+#include <Windows.h>
 using namespace std;
 
 int main()
 {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
     cout << setw(10) << showpos << +4 << endl;
     cout << setw(15) << internal << -67.09 << endl;
     cout << setw(10) << right << +235 << endl;
